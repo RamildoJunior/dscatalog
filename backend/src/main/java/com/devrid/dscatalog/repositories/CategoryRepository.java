bb871fe538738ab3df12e36fc6a5720df20a9840
RamildoJunior <rpsjuninho@gmail.com>
2025-08-19 20:42:59 -0300

    Evolução do projeto

---
+++ backend/src/main/java/com/devrid/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.devrid.dscatalog.repositories;
+
+import com.devrid.dscatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ backend/src/main/java/com/devrid/dscatalog/services/CategoryService.java
@@ -0,0 +1,18 @@
+package com.devrid.dscatalog.services;
+
+import com.devrid.dscatalog.entities.Category;
+import com.devrid.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+}
