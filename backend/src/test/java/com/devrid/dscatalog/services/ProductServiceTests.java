ae3e2c4565259e571cae1f4b014b4ec400eae299
RamildoJunior <rpsjuninho@gmail.com>
2025-09-04 18:07:34 -0300

    Subindo test

---
+++ backend/src/test/java/com/devrid/dscatalog/services/ProductServiceTests.java
@@ -0,0 +1,19 @@
+package com.devrid.dscatalog.services;
+
+import com.devrid.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService service;
+
+    @Mock
+    private ProductRepository repository;
+
+
+}
